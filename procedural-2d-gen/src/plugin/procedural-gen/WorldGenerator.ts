6d785241788ab277bb1bd483d118489fe871f654
noname <48761044+noname0310@users.noreply.github.com>
2022-09-25 19:02:22 +0900

    lint

--- procedural-2d-gen/src/plugin/procedural-gen/WorldGenerator.ts
+++ procedural-2d-gen/src/plugin/procedural-gen/WorldGenerator.ts
@@ -2,7 +2,6 @@ import { Immutable } from "src/plugin/types/Immutable";
 
 import { CoroutineDispatcher } from "../coroutine/CoroutineDispatcher";
 import { CoroutineIterator } from "../coroutine/CoroutineIterator";
-import { Logger } from "../helper/Logger";
 import { Vector2 } from "../math/Vector2";
 import { ChunkLoader } from "./ChunkLoader";
 import { ITilemapRenderer } from "./ITilemapRenderer";
