4a3cbdb686bd7ad19d416da52816a75cc892eafa
SungU Kang <20sunrin129@sunrint.hs.kr>
2022-09-30 01:12:48 +0900

    oxquiz

---
+++ ox-quiz/src/plugin/coroutine/CoroutineProcessor.ts
@@ -0,0 +1,81 @@
+import { Coroutine } from "./Coroutine";
+import { Time } from "./Time";
+import { WaitForSeconds, WaitUntil, WaitWhile } from "./YieldInstruction";
+
+/** @internal */
+export class CoroutineProcessor {
+    private readonly _time: Time;
+    private readonly _coroutines: (Coroutine|null)[];
+    private _coroutineCount: number;
+
+    private static readonly _needToCompactCount = 16;
+
+    public constructor(time: Time) {
+        this._time = time;
+        this._coroutines = [];
+        this._coroutineCount = 0;
+    }
+
+    public addCoroutine(coroutine: Coroutine): void {
+        this._coroutines.push(coroutine);
+        this._coroutineCount += 1;
+    }
+
+    public removeCoroutine(coroutine: Coroutine): void {
+        const index = this._coroutines.indexOf(coroutine);
+        if (index >= 0) {
+            this._coroutines[index] = null;
+            this._coroutineCount -= 1;
+        }
+    }
+
+    public tryCompact(): void {
+        const coroutines = this._coroutines;
+        if (CoroutineProcessor._needToCompactCount <= coroutines.length - this._coroutineCount) {
+            let insertPosition = 0;
+            for (let i = 0; i < coroutines.length; ++i) {
+                const coroutine = coroutines[i];
+                if (coroutine === null) continue;
+                coroutines[insertPosition] = coroutine;
+                insertPosition += 1;
+            }
+            coroutines.length = this._coroutineCount;
+        }
+    }
+
+    public process(): void {
+        const coroutines = this._coroutines;
+        for (let i = 0; i < coroutines.length; ++i) {
+            const coroutine = coroutines[i];
+            if (coroutine === null) continue;
+
+            if (coroutine.currentYieldInstructionExist) {
+                this.updateAfterProcessSingleInstruction(coroutine);
+            } else {
+                coroutines[i] = null;
+                this._coroutineCount -= 1;
+            }
+        }
+    }
+
+    private updateAfterProcessSingleInstruction(coroutine: Coroutine): void {
+        const currentYieldInstruction = coroutine.currentYieldInstruction;
+        if (currentYieldInstruction instanceof WaitForSeconds) {
+            coroutine.elapsedTime += this._time.deltaTime;
+            if (coroutine.elapsedTime >= currentYieldInstruction.seconds) {
+                coroutine.elapsedTime = 0;
+                coroutine.fatchNextInstruction();
+            }
+        } else if (currentYieldInstruction instanceof WaitUntil) {
+            if (currentYieldInstruction.predicate()) {
+                coroutine.fatchNextInstruction();
+            }
+        } else if (currentYieldInstruction instanceof WaitWhile) {
+            if (!currentYieldInstruction.predicate()) {
+                coroutine.fatchNextInstruction();
+            }
+        } else if (currentYieldInstruction === null) {
+            coroutine.fatchNextInstruction();
+        }
+    }
+}
---
+++ ox-quiz/src/plugin/index.ts
@@ -0,0 +1,116 @@
+import { IframeInfo, PluginEnvironmentInfo } from "theworld-client/types/plugin/declarations";
+import { Logger } from "./helper/Logger";
+
+enum OX {
+    O = 1,
+    X = 0,
+}
+
+class Plugin extends BasePlugin {
+    private _iframeInfo: IframeInfo|null = null;
+    private readonly _players = new Map<string, [number, number]>();
+    private judge_x = 0;
+
+    private question = "";
+    private playing = false;
+
+    private answer = OX.X;
+
+    private _broadcastPublicInfomation() {
+        this.broadcastMessage("pub_state",
+            this.question,
+            this.playing
+        )
+    }
+
+    private _broadcastPrivateInfomation() {
+        this.broadcastMessage("priv_state",
+            this.question,
+            this.playing,    
+            this.answer,
+        )
+    }
+
+    public override onLoad(_: any, environmentInfo: PluginEnvironmentInfo): void {
+        Logger.init(this);
+        try {
+            if(!environmentInfo.isLocal) throw new Error("Global not supported");
+            this._iframeInfo = environmentInfo.iframe;
+
+            this.judge_x = this._iframeInfo.x + this._iframeInfo.width/2;
+
+        } catch(e: any) {
+            Logger.error(`${e.message}\n${e.stack}`);
+        }
+    }
+    
+    public override onPlayerJoin(userId: string): void {
+        try {
+            let x = 987654321,y = 987654321;
+            if (this._iframeInfo) {
+                x = this._iframeInfo.x + 1;
+                y = this._iframeInfo.y + 1;
+            }
+            this._players.set(userId, [x, y]);
+        } catch(e: any) {
+            Logger.error(`${e.message}\n${e.stack}`);
+        }
+    }
+
+    public override onPlayerLeave(userId: string): void {
+        try {
+            this._players.delete(userId);
+        } catch (e: any) {
+            Logger.error(`${e.message}\n${e.stack}`);
+        }
+    }
+
+    public onPlayerMove(userId: string, x: number, y: number): void {
+        this._players.set(userId, [x, y]);
+    }
+
+    private _judge() {
+        if(this._iframeInfo === null) return;
+        const info = this._iframeInfo;
+
+        this._players.forEach(([x, y], userId) => {
+            if(!(
+                info.x <= x
+                && x < info.x + info.width
+                && info.y <= y
+                && y < info.y + info.height )) return;
+            
+            const player_select = (x >= this.judge_x) ? OX.X : OX.O;
+            
+            
+            if (player_select !== this.answer) {
+                this.teleportPlayer(
+                    userId, 
+                    info.x + info.width + 1,
+                    info.y + info.height - 2,
+                );
+            }
+        });
+    }
+
+    public override onChat(userId: string, message: string): void {
+        if(!this.isAdmin(userId)) return;
+        
+        if (/정답:(O|X)/gi.test(message)) {
+            this.playing = false;
+            const a = message.slice(3);
+            this.answer = (a[0]==="O") ? OX.O : OX.X;
+            this._judge();
+            this._broadcastPrivateInfomation();
+            return;
+        }
+        else if (/문제:+./gi.test(message)) {
+            this.playing = true;
+            const q = message.slice(3);
+            this.question = q;
+        }
+        this._broadcastPublicInfomation();
+    }
+}
+
+globalThis.PluginImpl = Plugin;
